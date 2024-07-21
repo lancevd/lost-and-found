bdffcd4f4d5c9de04d9b85e348bc39b77616fa19
lancevd <olamosob@gmail.com>
2024-07-21 01:42:10 +0100

    aside

--- src/app/post/[...details]/page.jsx
+++ src/app/post/[...details]/page.jsx
@@ -59,6 +59,29 @@ const page = () => {
               before committing to an actual design project.
             </p>
           </article>
+          {/* ASIDE */}
+          <article className="max-w-xs">
+            <a href="#">
+              <img
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-1.png"
+                className="mb-5 rounded-lg"
+                alt="Image 1"
+              />
+            </a>
+            <h2 className="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+              <a href="#">Our first office</a>
+            </h2>
+            <p className="mb-4 text-gray-500 dark:text-gray-400">
+              Over the past year, Volosoft has undergone many changes! After
+              months of preparation.
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline"
+            >
+              Read in 2 minutes
+            </a>
+          </article>
         </div>
       </main>
 
